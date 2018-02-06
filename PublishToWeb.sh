7bfe8c701c2ac34893ff877d2bd159dfc7f7bfeb
Andy Schwieter <andy@aschwieter.com>
2018-02-06 13:48:27 -0500

    added Google Analytics tracking

--- PublishToWeb.sh
+++ PublishToWeb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 # mv .\web\index.html .\web\old.html
-pandoc -o web/index.html -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css css/pandoc.css
+pandoc -o web/index.html -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css css/pandoc.css -H '.\header.html'
 pandoc -o web/pdfs/CCHMC_Style_Guide.pdf -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css ~/OneDrive\ -\ cchmc/Code/StyleGuide/web/css/pandoc.css --pdf-engine=wkhtmltopdf --variable=papersize:letter
 java -jar ~/OneDrive\ -\ cchmc/Programs/daisydiff-1.2/daisydiff.jar web/old.html web/index.html --file=web/diff.html
 surge --project ./web
