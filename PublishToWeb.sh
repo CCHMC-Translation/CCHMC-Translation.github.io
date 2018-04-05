fd64c56f3bbfbf42078dca9d517991589f657abe
Andy Schwieter <andy@aschwieter.com>
2018-04-05 13:10:17 -0400

    Added (LANGUAGE) for forms and moved PDF to DropBox

--- PublishToWeb.sh
+++ PublishToWeb.sh
@@ -2,5 +2,6 @@
 # mv .\web\index.html .\web\old.html
 pandoc -o web/index.html -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css css/pandoc.css -H '.\header.html' --metadata date="`date +%F`"
 pandoc -o web/pdfs/CCHMC_Style_Guide.pdf -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css css/pandoc.css --pdf-engine=wkhtmltopdf --variable=papersize:letter --metadata date="`date +%F`"
+cp web/pdfs/CCHMC_Style_Guide.pdf ~/Dropbox/CCHMC_Style_Guide.pdf
 java -jar ~/OneDrive\ -\ cchmc/Programs/daisydiff-1.2/daisydiff.jar web/old.html web/index.html --file=web/diff.html --css=css/pandoc.css
 surge --project ./web
