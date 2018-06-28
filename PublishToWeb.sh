a4baae0fee4ef0f972473796640279483ebf53cb
Andy Schwieter <andy@aschwieter.com>
2018-06-28 08:43:52 -0400

    published vendor instructions & fixed HTML css path

--- PublishToWeb.sh
+++ PublishToWeb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 # mv .\web\index.html .\web\old.html
 pandoc -o web/index.html -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css  "c:\Users\schzs8\OneDrive - cchmc\Code\StyleGuide\web\css\pandoc.css" -H '.\header.html' --metadata date="`date +%F`"
-pandoc -o web/vendor_instructions.html -f markdown+fancy_lists -s '.\vendor_instructions.md' --css  "c:\Users\schzs8\OneDrive - cchmc\Code\StyleGuide\web\css\pandoc.css" -H '.\header.html' --metadata date="`date +%F`"
+pandoc -o web/vendor_instructions.html -f markdown+fancy_lists -s '.\vendor_instructions.md' --css  "css/pandoc.css" -H '.\header.html' --metadata date="`date +%F`"
 pandoc -o web/pdfs/CCHMC_Style_Guide.pdf -f markdown+fancy_lists -s '.\Translation Style Guide for Cincinnati Childrens.md' --css "c:\Users\schzs8\OneDrive - cchmc\Code\StyleGuide\web\css\pandoc.css" --pdf-engine=wkhtmltopdf --variable=papersize:letter --metadata date="`date +%F`"
 pandoc -o web/pdfs/CCHMC_Vendor_Instructions.pdf -f markdown+fancy_lists -s '.\vendor_instructions.md' --css  "c:\Users\schzs8\OneDrive - cchmc\Code\StyleGuide\web\css\pandoc.css" --pdf-engine=wkhtmltopdf --variable=papersize:letter --metadata date="`date +%F`"
 cp web/pdfs/CCHMC_Style_Guide.pdf ~/Dropbox/CCHMC_Style_Guide.pdf
